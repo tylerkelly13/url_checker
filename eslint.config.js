d93fe0502b1a4b0688cb4a9bc3ab7b25308c91a6
Tyler Kelly <tyler.kelly@outlook.com>
2026-01-11 22:43:47 +1000

    get eslint and prettier to play nicely together

---
+++ eslint.config.js
@@ -0,0 +1,42 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import typescriptPlugin from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module'
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2021
+      }
+    },
+    plugins: {
+      '@typescript-eslint': typescriptPlugin,
+      prettier: prettierPlugin
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      camelcase: 'error',
+      'func-style': [
+        'error',
+        'declaration',
+        {
+          allowArrowFunctions: true
+        }
+      ],
+      'prefer-const': 'error'
+    }
+  },
+  prettierConfig
+];
